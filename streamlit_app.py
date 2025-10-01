22bf7148a1a79a3d1dd2c15392ea3df40998dca9
xoalss <xoalss@goedu.kr>
2025-10-01 09:21:45 +0000

    456

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("🎈 범박이의 첫 번째 앱! ")
 
 
-st.success(".")
-st.info(".")
+st.success("일리아")
+st.info("토푸리아")
 st.image("https://i.namu.wiki/i/cWV-60JSKEwSOQZLtIFTX68hWuRMdZLsFNYkV2iTHiZ25cJl0rEeE4GBSEvUgy_mmdN6GHFBSogvfGCNtW7fkA.webp")
